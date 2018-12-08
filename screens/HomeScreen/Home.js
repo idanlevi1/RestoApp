018592e605a596e89ecceb0a266f41b4ba44e301
idanlevi <idanlevi2@gmail.com>
2018-12-08 20:08:57 +0200

    split Home Screen

--- screens/HomeScreen/Home.js
+++ screens/HomeScreen/Home.js
@@ -1,35 +1,19 @@
 import React from 'react';
-import { Image, StyleSheet, View } from "react-native";
-import { MonoText } from '../../components/StyledText';
-import { QED_Group } from "../../constants/Colors";
+import HomeView from './HomeView'
+import { connect } from 'react-redux';
 
-export default class HomeScreen extends React.Component {
+class Home extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MonoText style={[styles.title,{
-          color: QED_Group.three,
-          borderLeftColor: QED_Group.four}]}>Wolcome!</MonoText>
-        <MonoText style={[styles.title,{
-          borderLeftWidth: 0,
-          color: QED_Group.one}]}>HomeScreen!</MonoText>
-      </View>
+      <HomeView user={this.props.user}/>
     );
   }
-
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: QED_Group.two,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 50,
-    fontWeight: "500",
-    paddingHorizontal: 15,
-    borderLeftWidth: 5,
-  },
-});
+const mapStateToProps = state => {
+  return {
+      user: state.user.user //We don't user 'user' for now, we can return {} if we want 
+  }
+}
+
+export default connect(mapStateToProps)(Home);
---
+++ screens/HomeScreen/HomeView.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, View } from "react-native";
+import { MonoText } from '../../components/StyledText';
+import { QED_Group } from "../../constants/Colors";
+
+class HomeView extends React.Component {
+  render() {
+    const {name,phone,email,avatarUrl} = this.props.user
+    return (
+        <View style={styles.container}>
+            <MonoText style={[styles.title,{
+            color: QED_Group.three,
+            borderLeftColor: QED_Group.four}]}>Wolcome!</MonoText>
+            <MonoText style={[styles.title,{
+            borderLeftWidth: 0,
+            color: QED_Group.one}]}>{name}</MonoText>
+        </View>
+    );
+  }
+}
+
+export default HomeView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: QED_Group.two,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: "500",
+    paddingHorizontal: 15,
+    borderLeftWidth: 5,
+  },
+});
